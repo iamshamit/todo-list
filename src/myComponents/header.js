ff49310d6c2cddebf9fd797e833be2ae1cf05ba5
Shamit Mishra <82385464+itzshamit@users.noreply.github.com>
2023-07-30 23:43:09 +0530

    url

--- src/myComponents/header.js
+++ src/myComponents/header.js
@@ -6,7 +6,7 @@ export default function Header(procs) {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
+        <Link className="navbar-brand" to="todo-list/">
           {procs.title}
         </Link>
         <button
@@ -23,12 +23,12 @@ export default function Header(procs) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">
+              <Link className="nav-link active" aria-current="page" to="todo-list/">
                 Home
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/about">
+              <Link className="nav-link" to="todo-list/about">
                 About
               </Link>
             </li>
