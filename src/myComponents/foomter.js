3a86b77ef5ef75893b1b8e7cf2203d4f9ca0f182
Shamit Mishra <82385464+iamshamit@users.noreply.github.com>
2024-06-19 17:03:53 +0530

    lol

--- src/myComponents/foomter.js
+++ src/myComponents/foomter.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-export default function Foomter() {
+export default function Footer() {
   return (
-    <footer className="bg-dark text-light text-center py-2" >
-    <p>Copyright &copy; TodoListX</p>
+    <footer className="bg-dark text-light  py-3">
+      <p className="my-auto mx-5">
+        <span className="mx-5">Copyright &copy; TodoListX</span>
+        <span className="mx-5" style={{ float: 'right' }}>Made with love by Shamit ♥</span>
+      </p>
     </footer>
   );
 }
